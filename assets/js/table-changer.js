b3d55630382011eb65c228ab544f62f934fc4402
lestherxm <lestherxm@gmail.com>
2023-11-29 11:05:16 -0600

    toggle table coins and commissions support with JS

--- assets/js/table-changer.js
+++ assets/js/table-changer.js
@@ -1,14 +1,15 @@
-    let btnToCommissions = document.getElementById("to-commissions");
-    let btnToCoins = document.getElementById("to-coins");
+let btnToCommissions = document.getElementById('to-commissions');
+let btnToCoins = document.getElementById('to-coins');
 
-    let tblCoins = document.getElementById("tbl-coins");
-    let tblCommissions = document.getElementById("tbl-commissions");
+let tblCoins = document.getElementById('tbl-coins');
+let tblCommissions = document.getElementById('tbl-commissions');
 
-    btnToCommissions.addEventListener("click", () => {
-        /* SHIFT + ALT + A */
-        /* Hide tbl Coins */
-        tblCommissions.classList.toggle("hide-element");
-        btnToCommissions.classList.toggle("hide-element");
-        /* Show tbl Commissions */
-        tblCoins.classList.toggle("hide-element");
-    });
+btnToCommissions.addEventListener('click', toggleTables);
+btnToCoins.addEventListener('click', toggleTables)
+
+function toggleTables(){
+    tblCoins.classList.toggle('hide-element');
+    tblCommissions.classList.toggle('hide-element');
+    btnToCommissions.classList.toggle('hide-element');
+    btnToCoins.classList.toggle('hide-element');
+}
